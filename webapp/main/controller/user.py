d3c847d6aaa9a4d611bb85d5b438637676d41c58
Björn Kristensson Alfsson <Bjorn.KristenssonAlfsson@alten.se>
2019-02-06 13:30:47 +0100

    Add user works

--- webapp/main/controller/user.py
+++ webapp/main/controller/user.py
@@ -1,13 +1,18 @@
-from sqlalchemy import create_engine
-from flask import jsonify, Blueprint
+from sqlalchemy import create_engine, orm
+from flask import jsonify, Blueprint, request
 from ..config.postgres import postgres
-from datetime import datetime
 from ..model.models import User
+from datetime import datetime
 
 user_api = Blueprint('user_api', __name__)
 
 engine = create_engine(postgres)
 connection = engine.connect()
+
+Session = orm.sessionmaker(bind=engine)
+session = Session()
+
+dt = datetime.now()
 
 
 @user_api.route('/')
@@ -15,13 +20,27 @@ def list_all_users():
 
     results = connection.execute("SELECT * FROM temp")
 
-    return jsonify([dict(row) for row in results])
+    return jsonify([dict(row) for row in results]), 200
 
 
-@user_api.route('/add', methods = ['POST'])
+@user_api.route('/add', methods=['POST'])
 def add_user():
-    print("HERE")
-    dt = datetime.now()
-    new_user = User.insert()
+    dict_body = request.get_json()
 
-    new_user.execute(name='Test', created_on=dt)
+    user = User(user=dict_body['name'],
+                created_on=dt)
+
+    session.add(user)
+    session.commit()
+    return jsonify({'message': 'New user was added.'}), 200
+
+
+@user_api.route('/delete', methods=['DELETE'])
+def delete_user():
+    user_id = request.args.get('id')
+
+    user = User.query.filter_by(id=user_id).first()
+
+    session.delete(user)
+    session.commit()
+    return jsonify({'message': 'A user was deleted.'}), 200
--- webapp/main/model/models.py
+++ webapp/main/model/models.py
@@ -1,16 +1,16 @@
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
 
 db = SQLAlchemy()
 
 
-class User:
+class User(db.Model):
     __tablename__ = 'temp'
 
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    id = db.Column('id', db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(75), nullable=False)
     created_on = db.Column(db.DateTime)
 
-    def __init__(self, user):
-        self.user = user
-        self.created_on = datetime.now()
+    def __init__(self, user, created_on):
+        self.name = user
+        self.created_on = created_on
+        super(User, self).__init__()
